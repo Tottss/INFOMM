a7520bd4ba1184b1fd47b0d5b0bfa9eda7364046
nakaraan <namh.b1l6@gmail.com>
2025-03-21 01:19:42 +0800

    decapitalized

--- src/java/clinicdb/Doctor.java
+++ src/java/clinicdb/Doctor.java
@@ -3,99 +3,99 @@ package java.clinicdb;
 import java.sql.*;
 
 public class Doctor {
-    public String NPI;
-    public String Last_name;
-    public String First_name;
-    public String Middle_name;
-    public String Sex;
-    public String Birthdate;
-    public String Medical_certification;
-    public String Year_of_service;
-    public String Specialization;
+    public String npi;
+    public String last_name;
+    public String first_name;
+    public String middle_name;
+    public String sex;
+    public String birthdate;
+    public String medical_certification;
+    public String year_of_service;
+    public String specialization;
 
     public Doctor() {
-        NPI = "";
-        Last_name = "";
-        First_name = "";
-        Middle_name = "";
-        Sex = "";
-        Birthdate = "";
-        Medical_certification = "";
-        Year_of_service = "";
-        Specialization = "";
+        npi = "";
+        last_name = "";
+        first_name = "";
+        middle_name = "";
+        sex = "";
+        birthdate = "";
+        medical_certification = "";
+        year_of_service = "";
+        specialization = "";
     }
 
     // Getters and Setters
     public String getNPI() {
-        return NPI;
+        return npi;
     }
 
-    public void setNPI(String NPI) {
-        this.NPI = NPI;
+    public void setNPI(String npi) {
+        this.npi = npi;
     }
 
     public String getLastName() {
-        return Last_name;
+        return last_name;
     }
 
-    public void setLastName(String Last_name) {
-        this.Last_name = Last_name;
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
     }
 
     public String getFirstName() {
-        return First_name;
+        return first_name;
     }
 
-    public void setFirstName(String First_name) {
-        this.First_name = First_name;
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
     }
 
     public String getMiddleName() {
-        return Middle_name;
+        return middle_name;
     }
 
-    public void setMiddleName(String Middle_name) {
-        this.Middle_name = Middle_name;
+    public void setMiddleName(String middle_name) {
+        this.middle_name = middle_name;
     }
 
     public String getSex() {
-        return Sex;
+        return sex;
     }
 
-    public void setSex(String Sex) {
-        this.Sex = Sex;
+    public void setSex(String sex) {
+        this.sex = sex;
     }
 
     public String getBirthdate() {
-        return Birthdate;
+        return birthdate;
     }
 
-    public void setBirthdate(String Birthdate) {
-        this.Birthdate = Birthdate;
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
     }
 
     public String getMedicalCertification() {
-        return Medical_certification;
+        return medical_certification;
     }
 
-    public void setMedicalCertification(String Medical_certification) {
-        this.Medical_certification = Medical_certification;
+    public void setMedicalCertification(String medical_certification) {
+        this.medical_certification = medical_certification;
     }
 
     public String getYearsOfService() {
-        return Year_of_service;
+        return year_of_service;
     }
 
-    public void setYearsOfService(String Year_of_service) {
-        this.Year_of_service = Year_of_service;
+    public void setYearsOfService(String year_of_service) {
+        this.year_of_service = year_of_service;
     }
 
     public String getSpecialization() {
-        return Specialization;
+        return specialization;
     }
 
-    public void setSpecialization(String Specialization) {
-        this.Specialization = Specialization;
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
     }
 
     public boolean adddoctor() {
@@ -103,16 +103,16 @@ public class Doctor {
             Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/dbathletes?useTimezone=true&serverTimezone=UTC&user=root&password=1123_Jeru");
             PreparedStatement pstmt = conn.prepareStatement(
-                    "INSERT INTO Doctor (NPI, Last_name, First_name, Middle_name, Sex, Birthdate, Medical_certification, Years_of_service, Specialization) VALUES (?, ?, ?, ?, ?, ?, ?, ? ,?)");
-            pstmt.setString(1, NPI);
-            pstmt.setString(2, Last_name);
-            pstmt.setString(3, First_name);
-            pstmt.setString(4, Middle_name);
-            pstmt.setString(5, Sex);
-            pstmt.setString(6, Birthdate);
-            pstmt.setString(7, Medical_certification);
-            pstmt.setString(8, Year_of_service);
-            pstmt.setString(9, Specialization);
+                    "INSERT INTO Doctor (npi, last_name, first_name, middle_name, sex, birthdate, medical_certification, Years_of_service, specialization) VALUES (?, ?, ?, ?, ?, ?, ?, ? ,?)");
+            pstmt.setString(1, npi);
+            pstmt.setString(2, last_name);
+            pstmt.setString(3, first_name);
+            pstmt.setString(4, middle_name);
+            pstmt.setString(5, sex);
+            pstmt.setString(6, birthdate);
+            pstmt.setString(7, medical_certification);
+            pstmt.setString(8, year_of_service);
+            pstmt.setString(9, specialization);
             pstmt.executeUpdate();
             pstmt.close();
             conn.close();
@@ -123,7 +123,7 @@ public class Doctor {
         }
     }
 
-    public boolean updateDoctorField(String NPI, String field, String newValue) {
+    public boolean updateDoctorField(String npi, String field, String newValue) {
         // Validate input to prevent SQL injection (only allow specific column names)
         if (!isValidField(field)) {
             System.out.println("Invalid field name.");
@@ -131,14 +131,14 @@ public class Doctor {
         }
 
         // Dynamic SQL Query
-        String sql = "UPDATE Doctor SET " + field + " = ? WHERE NPI = ?";
+        String sql = "UPDATE Doctor SET " + field + " = ? WHERE npi = ?";
 
         try (Connection conn = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/dbathletes?useTimezone=true&serverTimezone=UTC&user=root&password=1123_Jeru");
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
             pstmt.setString(1, newValue); // New value
-            pstmt.setString(2, NPI); // Doctor's ID
+            pstmt.setString(2, npi); // Doctor's ID
 
             int rowsUpdated = pstmt.executeUpdate();
             return rowsUpdated > 0;
@@ -153,8 +153,8 @@ public class Doctor {
         try {
             Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/dbathletes?useTimezone=true&serverTimezone=UTC&user=root&password=1123_Jeru");
-            PreparedStatement pstmt = conn.prepareStatement("DELETE FROM Doctor WHERE NPI=?");
-            pstmt.setString(1, NPI);
+            PreparedStatement pstmt = conn.prepareStatement("DELETE FROM Doctor WHERE npi=?");
+            pstmt.setString(1, npi);
             pstmt.executeUpdate();
             System.out.println("Doctor record was deleted");
             pstmt.close();
@@ -170,20 +170,20 @@ public class Doctor {
         try {
             Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/dbapp?useSSL=false&serverTimezone=UTC", "root", "sgdsmt21");
-            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM doctor WHERE NPI=?");
-            pstmt.setString(1, NPI);
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM doctor WHERE npi=?");
+            pstmt.setString(1, npi);
             ResultSet rs = pstmt.executeQuery();
 
             while (rs.next()) {
-                NPI = rs.getString("NPI");
-                Last_name = rs.getString("Last_name");
-                First_name = rs.getString("First_name");
-                Middle_name = rs.getString("Middle_name");
-                Sex = rs.getString("Sex");
-                Birthdate = rs.getString("Birthdate");
-                Medical_certification = rs.getString("Medical_certification");
-                Year_of_service = rs.getString("Year_of_service");
-                Specialization = rs.getString("Specialization");
+                npi = rs.getString("npi");
+                last_name = rs.getString("last_name");
+                first_name = rs.getString("first_name");
+                middle_name = rs.getString("middle_name");
+                sex = rs.getString("sex");
+                birthdate = rs.getString("birthdate");
+                medical_certification = rs.getString("medical_certification");
+                year_of_service = rs.getString("year_of_service");
+                specialization = rs.getString("specialization");
 
                 // front end code nalang kulang
                 rs.close();
@@ -203,7 +203,7 @@ public class Doctor {
     // Helper method to validate allowed column names (prevents SQL injection)
     private boolean isValidField(String field) {
         return field.matches(
-                "Last_name|First_name|Middle_name|Sex|Birthdate|Medical_certification|Years_of_service|Specialization");
+                "last_name|first_name|middle_name|sex|birthdate|medical_certification|Years_of_service|specialization");
     }
 
 }
