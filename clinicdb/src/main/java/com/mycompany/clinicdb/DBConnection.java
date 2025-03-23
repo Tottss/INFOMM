fff0a01ceb917cbdec0f0c4c4cc81091f6c0ffe6
nite29 <158014309+nite29@users.noreply.github.com>
2025-03-23 18:02:55 +0800

    Delete clinicdb directory

--- clinicdb/src/main/java/com/mycompany/clinicdb/DBConnection.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.clinicdb;
-
-public class DBConnection {
-    public static final String URL = "jdbc:mysql://localhost:3306/clinic?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
-    public static final String USER = "root";
-    public static final String PASSWORD = "1123_Jeru";
-}
